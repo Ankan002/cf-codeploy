ec61c21269f6f451e7843aa5358864091dd40121
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-02-10 05:00:18 +0530

    feat: :sparkles: add dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { MainNavbar } from "@/components/common";
+
+const DashboardPage = () => {
+	return (
+		<>
+			<MainNavbar />
+			<main className="flex min-h-screen flex-col bg-black pt-14"></main>
+		</>
+	);
+};
+
+export default DashboardPage;
