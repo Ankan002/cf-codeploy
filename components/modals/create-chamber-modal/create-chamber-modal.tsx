b81fe9bd090e25a819f1dfe6edcf907dcfb63919
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-02-11 04:37:56 +0530

    feat: :sparkles: add horizontal scroll

--- components/modals/create-chamber-modal/create-chamber-modal.tsx
+++ components/modals/create-chamber-modal/create-chamber-modal.tsx
@@ -3,6 +3,7 @@
 import ReactModal from "react-modal";
 import { useCreateChamberModal } from "./hook";
 import { SolidBtn, TextInput } from "@/components/elements";
+import { X } from "lucide-react";
 
 interface Props {
 	title: string;
@@ -13,7 +14,15 @@ interface Props {
 const CreateChamberModal = (props: Props) => {
 	const { title, isModalOpen, onModalCloseRequest } = props;
 
-	const { name, onNameChange } = useCreateChamberModal();
+	const {
+		name,
+		onNameChange,
+		currentEmail,
+		onCurrentEmailChange,
+		onInviteClick,
+		emailsInvited,
+		removeFromInvitedList,
+	} = useCreateChamberModal();
 
 	return (
 		<ReactModal
@@ -35,7 +44,7 @@ const CreateChamberModal = (props: Props) => {
 			className="w-full max-w-[600px] flex flex-col"
 		>
 			<div className="w-full flex flex-col border border-white rounded-md px-4 py-3 bg-black font-geist-sans">
-				<h1 className="text-xl text-white">Create Chamber</h1>
+				<h1 className="text-xl text-white">{title}</h1>
 
 				<TextInput
 					title="Name"
@@ -43,6 +52,44 @@ const CreateChamberModal = (props: Props) => {
 					onChange={onNameChange}
 					className="mt-4"
 				/>
+
+				<div className="flex flex-col mt-4">
+					<p className="text-sm text-white">Invited Emails</p>
+
+					<div className="w-full flex overflow-x-auto border border-light-grey py-1 px-1 rounded-md min-h-10 mt-1">
+						{emailsInvited.map((e, i) => (
+							<div
+								className="w-fit flex items-center px-1 py-1 rounded-md font-geist-sans bg-white mx-1 text-black"
+								key={i}
+							>
+								<p>{e}</p>
+
+								<button
+									className="ml-1"
+									onClick={() => removeFromInvitedList(i)}
+								>
+									<X size={16} />
+								</button>
+							</div>
+						))}
+					</div>
+				</div>
+
+				<div className="mt-4 flex items-center">
+					<TextInput
+						title=""
+						value={currentEmail}
+						onChange={onCurrentEmailChange}
+						placeholder="Invited Email"
+					/>
+
+					<SolidBtn
+						title="Invite"
+						className="border border-white bg-white text-black rounded-md w-fit py-2 ml-3"
+						titleClassName="text-black"
+						onClick={onInviteClick}
+					/>
+				</div>
 
 				<div className="w-full flex items-end justify-end mt-4">
 					<SolidBtn
--- components/modals/create-chamber-modal/hook.ts
+++ components/modals/create-chamber-modal/hook.ts
@@ -3,9 +3,22 @@ import { useState } from "react";
 
 export const useCreateChamberModal = () => {
 	const [name, setName] = useState<string>("");
+	const [currentEmail, setCurrentEmail] = useState<string>("");
+	const [emailsInvited, setEmailsInvited] = useState<Array<string>>([]);
+
+	const onInviteClick = () => {
+		setEmailsInvited((prev) => [...prev, currentEmail]);
+		setCurrentEmail("");
+	};
 
 	return {
 		name,
 		onNameChange: textInputController(setName),
+		currentEmail,
+		onCurrentEmailChange: textInputController(setCurrentEmail),
+		emailsInvited,
+		onInviteClick,
+		removeFromInvitedList: (i: number) =>
+			setEmailsInvited((prev) => prev.filter((_, index) => index !== i)),
 	};
 };
